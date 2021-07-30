c0b7befb757967a1a2c407db92150268551e45ff
superblackwoo <862231364@qq.com>
2021-07-30 11:34:12 +0800

    update

---
+++ 09_myAbstract/src/a01/Animal.java
@@ -0,0 +1,25 @@
+package a01;
+
+public abstract class Animal {
+
+    //抽象类可以有成员变量
+    private int age = 20;
+    private final String city = "beijing";
+
+    //抽象类可以有构造方法
+    public Animal(int age) {
+        this.age = age;
+    }
+
+    public Animal() {
+    }
+
+
+    //抽象类可以有非抽象成员函数
+    public void show(){
+        System.out.println(age);
+        System.out.println(city);
+    }
+
+    public abstract void eat();
+}
