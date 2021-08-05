c5f12035656db0bd08190b9a8449227dba3cac70
superblackwoo <862231364@qq.com>
2021-08-05 15:42:18 +0800

    update

--- 10_myInterface/src/num5_maopaopaixu/popSort.java
+++ 10_myInterface/src/num5_maopaopaixu/popSort.java
@@ -2,9 +2,78 @@ package num5_maopaopaixu;
 
 public class popSort {
     public static void main(String[] args) {
-        int [] a = {24,25,89,3,6,35,9};
+        int[] a = {99, 98, 24, 25, 89, 3, 6, 35, 9};
         //String s = atoString(a);
         System.out.println("排序前：" + atoString(a));
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                int temp = a[i + 1];
+                a[i + 1] = a[i];
+                a[i] = temp;
+            }
+        }
+        System.out.println("第一次排序后：" + atoString(a));
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                int temp = a[i + 1];
+                a[i + 1] = a[i];
+                a[i] = temp;
+            }
+        }
+        System.out.println("第二次排序后：" + atoString(a));
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                int temp = a[i + 1];
+                a[i + 1] = a[i];
+                a[i] = temp;
+            }
+        }
+        System.out.println("第三次排序后：" + atoString(a));
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                int temp = a[i + 1];
+                a[i + 1] = a[i];
+                a[i] = temp;
+            }
+        }
+        System.out.println("第四次排序后：" + atoString(a));
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                int temp = a[i + 1];
+                a[i + 1] = a[i];
+                a[i] = temp;
+            }
+        }
+        System.out.println("第五次排序后：" + atoString(a));
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                int temp = a[i + 1];
+                a[i + 1] = a[i];
+                a[i] = temp;
+            }
+        }
+        System.out.println("第六次排序后：" + atoString(a));
+
+        int aaa[] = new int[999000];
+        double ba =System.currentTimeMillis();
+        for(int i = 0; i < aaa.length; i++){
+            aaa[i] = (int)(Math.random()*100);
+        }
+        double ca = System.currentTimeMillis();
+        System.out.println((ca-ba));
+
+
+        double b =System.currentTimeMillis();
+        popSort(aaa);
+        double c = System.currentTimeMillis();
+        System.out.println((c-b));
+        System.out.println(atoString(aaa));
     }
 
     //格式化输出整数数组
@@ -23,4 +92,19 @@ public class popSort {
         s += "]";
         return s;
     }
+
+    public static int[] popSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int x = 0; x < i; x++) {
+                if(a[x]>a[x+1]){
+                    int temp = a[x+1];
+                    a[x+1] = a[x];
+                    a[x] = temp;
+                }
+            }
+            //System.out.println("第"+(i+1)+"轮排序后："+ atoString(a));
+        }
+        return a;
+    }
+
 }
