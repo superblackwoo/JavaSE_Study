a71c95a3f02f7e1a7a5a9c4266d2b654264950eb
superblackwoo <862231364@qq.com>
2021-08-13 14:57:53 +0800

    update

--- 12_myIO/src/num4_charStream/Demo.java
+++ 12_myIO/src/num4_charStream/Demo.java
@@ -1,10 +1,32 @@
 package num4_charStream;
 
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Demo {
-    public static void main(String[] args) {
-        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("12_myIO\\test\\osw.txt"));
+    public static void main(String[] args) throws IOException {
+        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("12_myIO\\test\\osw.txt",true),"GBK");
+        osw.write("第一觉得卡拉点击按钮斯大林时代和");
+        osw.close();
+
+        InputStreamReader osr = new InputStreamReader(new FileInputStream("12_myIO\\test\\osw.txt"),"GBK");
+        int len;
+        while((len = osr.read()) != -1){
+            System.out.print((char) len);
+        }
+        osr.close();
+
+
+        //----------------------------------------------------------------------------
+        //复制java文件
+        OutputStreamWriter osw1 = new OutputStreamWriter(new FileOutputStream("12_myIO\\test\\After_Copy.java"));
+
+        InputStreamReader isr1  = new InputStreamReader(new FileInputStream("12_myIO\\test\\Demo1_CopyVideo.java"));
+
+        while((len=isr1.read()) != -1){
+            osw1.write(len);
+        }
+
+        osw1.close();
+        isr1.close();
     }
 }
