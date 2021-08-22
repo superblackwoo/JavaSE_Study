73416c8e6fb1ae58f4fe55041179ec87ae9fdce9
superblackwoo <862231364@qq.com>
2021-08-22 13:37:45 +0800

    update

--- 13_myThread/src/num2_SellTicket/Tickets_syn.java
+++ 13_myThread/src/num2_SellTicket/Tickets_syn.java
@@ -6,16 +6,18 @@ public class Tickets_syn implements Runnable {
 
     @Override
     public void run() {
-        while (tickets > 0) {
+        while (true) {
             //模拟出票时间
             synchronized (obj) { //一个对象对应一把锁
-                try {
-                    Thread.currentThread().sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                if (tickets > 0) {
+                    try {
+                        Thread.currentThread().sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "正在卖出第" + tickets + "张票");
+                    tickets--;
                 }
-                System.out.println(Thread.currentThread().getName() + "正在卖出第" + tickets + "张票");
-                tickets--;
             }
         }
     }
